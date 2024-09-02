dec46c88d3711b686a2412de2a7682521533fadc
Eric Brown <eric@ebrown.net>
2024-09-02 11:58:32 -0600

    Set networks to play each other

--- src/ai/self_play.py
+++ src/ai/self_play.py
@@ -5,6 +5,7 @@ from gymnasium import spaces, register
 from gymnasium.core import ActType, ObsType
 
 from src.ai.actions import action_to_move
+from src.ai.agent import CheckersAI
 from src.game.board import GameBoard, Player
 
 
@@ -42,8 +43,7 @@ class CheckersEnv(gym.Env):
     def step(
         self, action: ActType
     ) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
-        move = self.MOVES[action]
-        self.game_board.make_move(move)
+        self.game_board.make_move(action)
 
         terminated = self.game_board.game_over
         reward = self.game_board.scores[self.player]
@@ -58,3 +58,26 @@ register(
     max_episode_steps=100,
     reward_threshold=1.0,
 )
+
+
+if __name__ == "__main__":
+    env = CheckersEnv()
+    observation = env.reset()
+    terminated = False
+
+    agents = {
+        Player.RED: CheckersAI.init(Player.RED),
+        Player.BLACK: CheckersAI.init(Player.BLACK),
+    }
+    current_player = Player.RED
+
+    while not terminated:
+        action, _ = agents[current_player].select_action(env.game_board)
+        print(f"Player {current_player}: {action}")
+        print(env.game_board.board)
+        next_observation, reward, terminated, _, _ = env.step(action)
+        current_player = -current_player
+        observation = next_observation
+
+    print("Game over!")
+    print(env.game_board.scores)
