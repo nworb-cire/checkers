2c81827389c86259450796ab65da8f5fcec28b03
Eric Brown <eric@ebrown.net>
2024-09-06 10:32:50 -0600

    Add docstrings to BoardState class

--- src/game/board.py
+++ src/game/board.py
@@ -9,9 +9,19 @@ from src.game.settings import MUST_JUMP
 
 
 class BoardState:
+    """
+    Represents a state of the board, with some functions for getting available moves. The board is represented as
+    an 8x8 numpy array, +1 representing a red piece, +2 representing a red king, -1 representing a black piece, and -2
+    representing a black king. 0 represents an empty space.
+    """
+
     board: np.ndarray
 
     def __init__(self, board: np.ndarray | None = None):
+        """
+        Initialize the board state. If no board is provided, the default starting board is used.
+        :param board: An 8x8 numpy array representing the board state.
+        """
         if board is None:
             board = self.setup_board()
         assert board.shape == (8, 8)
@@ -21,18 +31,35 @@ class BoardState:
         return self.board[key]
 
     def __setitem__(self, key, value):
+        """
+        Place or remove a piece from the board. The key is a tuple representing the row and column of the piece, and the
+        value must be one of -2, -1, 0, 1, or 2.
+        :param key: Tuple representing the row and column of the piece.
+        :param value: Integer representing the piece to place or remove. Must be one of -2, -1, 0, 1, or 2.
+        """
         assert value in (-2, -1, 0, 1, 2)
         assert 0 <= key[0] < 8
         assert 0 <= key[1] < 8
+        assert sum(key) % 2 == 0  # only even squares
         self.board[key] = value
 
     def __repr__(self):
         return f"{self.__class__.__name__}({repr(self.board)})"
 
     def flip(self):
+        """
+        Flip the board, effectively rotating it 180 degrees and reversing the players (red becomes black and vice versa).
+        This is used to simplify AI implementations, so that the AI can always play as red.
+        :return: A new BoardState object representing the flipped board.
+        """
         return BoardState(-self.board[::-1, ::-1])
 
     def visualize(self):
+        """
+        Visualize the board as a string, with red pieces represented by "r" and black pieces represented by "b".
+        Kings are represented by captial letters. This is useful for debugging or for a CLI based game.
+        :return: A string representation of the board.
+        """
         s = ""
         for row in range(8):
             for col in range(8):
@@ -51,9 +78,23 @@ class BoardState:
         return s
 
     def __eq__(self, other):
+        """
+        Check if two board states are equal. This is used for unit testing.
+        :param other: A BoardState object to compare to.
+        :return: True if the board states are equal, False otherwise.
+        """
+        if not isinstance(other, BoardState):
+            return False
         return np.allclose(self.board, other.board)
 
     def get_available_moves(self, player: Player) -> tuple[list[Move], list[Move]]:
+        """
+        Get all available moves for a player. Returns a tuple of two lists, the first containing regular moves and the
+        second containing jump moves. If the game settings require that a player must jump if possible, only jump moves
+        will be returned if available.
+        :param player: The player to get moves for.
+        :return: A tuple of (moves, jump_moves).
+        """
         moves = []
         jump_moves = []
         for row in range(8):
@@ -74,6 +115,13 @@ class BoardState:
         return 6 if player == Player.RED else 1
 
     def get_moves(self, row: int, col: int, player: Player):
+        """
+        Get all available standard (non-jump) moves for the piece at the given row and column.
+        :param row: The row of the piece.
+        :param col: The column of the piece.
+        :param player: The player that owns the piece.
+        :return: A list of Move objects representing the available moves.
+        """
         moves = []
         if self.board[row, col] == 0:
             return moves
@@ -105,6 +153,13 @@ class BoardState:
         return moves
 
     def get_jump_moves(self, row: int, col: int, player: Player):
+        """
+        Get all available jump moves for the piece at the given row and column.
+        :param row: The row of the piece.
+        :param col: The column of the piece.
+        :param player: The player that owns the piece.
+        :return: A list of Move objects representing the available jump moves.
+        """
         moves = []
         if self.board[row, col] == 0:
             return moves
@@ -156,26 +211,48 @@ class BoardState:
         return moves
 
     def get_moves_mask(self, player: Player):
+        """
+        Get a boolean mask representing the available moves for a player. This is used to mask out invalid moves for AI.
+        :param player: The player to get moves for.
+        :return: A list of booleans representing the available moves. This has the same shape as the MOVES dictionary.
+        """
         moves, jump_moves = self.get_available_moves(player)
         mask = [move in moves + jump_moves for move in MOVES.values()]
         return mask
 
-    def is_able_to_move(self, player: Player):
+    def is_able_to_move(self, player: Player) -> bool:
+        """
+        Check if a player is able to move. This is used to determine if the game is over.
+        :param player: The player to check.
+        :return: True if the player has valid moves, False otherwise.
+        """
         moves, jump_moves = self.get_available_moves(player)
         return len(moves) + len(jump_moves) > 0
 
-    def is_stalemate(self):
+    def is_stalemate(self) -> bool:
+        """
+        Check if the game is in a stalemate, meaning that neither player has any valid moves.
+        :return: True if the game is in a stalemate, False otherwise.
+        """
         return not self.is_able_to_move(Player.RED) and not self.is_able_to_move(
             Player.BLACK
         )
 
-    def is_game_over(self):
+    def is_game_over(self) -> bool:
+        """
+        Check if the game is over, meaning that at least one player has no valid moves.
+        :return: True if the game is over, False otherwise.
+        """
         return not self.is_able_to_move(Player.RED) or not self.is_able_to_move(
             Player.BLACK
         )
 
     @staticmethod
     def setup_board():
+        """
+        Create the default starting board state.
+        :return: A np.ndarray representing the default starting board state.
+        """
         board = np.array(
             [
                 [1, 0, 1, 0, 1, 0, 1, 0],
